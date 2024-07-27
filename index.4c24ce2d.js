7d821ff4050087816e98a594b36d0f8e92a2c957
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-27 12:18:20 +0300

    make build

---
+++ index.4c24ce2d.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var a=t.target;if("IMG"===a.tagName||"A"===a.tagName){var n=("A"===a.tagName?a:a.parentElement).href;e.src=n,t.preventDefault()}});
+//# sourceMappingURL=index.4c24ce2d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const mainImg = document.getElementById("largeImg");
+const thumbsnailGallery = document.getElementById("thumbs");
+thumbsnailGallery.addEventListener("click", (e)=>{
+    const target = e.target;
+    if (target.tagName === "IMG" || target.tagName === "A") {
+        const thumbnailLink = target.tagName === "A" ? target : target.parentElement;
+        const newSrc = thumbnailLink.href;
+        mainImg.src = newSrc;
+        e.preventDefault();
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
